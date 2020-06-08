432d95e76db68d36dca93f18cbfa50e834588f10
mrtor114 <mrtor114@gmail.com>
2020-06-08 14:47:40 -0700

    Create Exercise4_Rachan_C.py
    complete python 3 ex 4

---
+++ Exercise4_Rachan_C.py
@@ -0,0 +1,12 @@
+#Year: 2 Semester: 1
+
+FE = 88.5
+GB = 95.5
+ICS = 77
+CP = 88
+
+print('--------------Your Score ---------------')
+print('Foundation English               :', FE)
+print('General Business                 :', GB)
+print('Introduction to Computer Systems :', ICS)
+print('Computer Programming             :', CP)
