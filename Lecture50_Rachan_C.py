fb9f0a883689ffdf3277bfbc50818ae90162ea85
mrtor114 <mrtor114@gmail.com>
2020-06-15 14:15:00 -0700

    Create Lecture50_Rachan_C.py
    lecture 50 function

---
+++ Lecture50_Rachan_C.py
@@ -0,0 +1,12 @@
+def addNumber(x,y):
+    print("Result:", x+y)
+def minusNumber(x,y):
+    print("Result:", x-y)
+def multipleNumber(x,y):
+    print("Result:", x*y)
+def devideNumber(x,y):
+    print("Result:", x/y)
+addNumber(5,9)
+minusNumber(3,7)
+multipleNumber(15,3)
+devideNumber(100,25)
