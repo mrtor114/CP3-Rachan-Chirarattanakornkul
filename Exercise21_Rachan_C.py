55b020e5eaf1a407f18912d2f3386e575b75bb15
mrtor114 <mrtor114@gmail.com>
2020-06-19 15:21:21 -0700

    Create Exercise21_Rachan_C.py
    BMI exercise

---
+++ Exercise21_Rachan_C.py
@@ -0,0 +1,36 @@
+from tkinter import *
+import math
+
+def leftClick(event):
+    bmi = (float(textBoxWeight.get())/math.pow((float(textBoxHeight.get())/100),2))
+    print('Your BMI is: ', bmi)
+    if bmi >=30:
+        labelResult.configure(text='DANGER!!')
+    elif bmi >=25:
+        labelResult.configure(text='Extreme Weight')
+    elif bmi >=23:
+        labelResult.configure(text='Over Weight')
+    elif bmi >= 18.6:
+        labelResult.configure(text='Good')
+    elif bmi <=18.5:
+        labelResult.configure(text='Under Weight')
+    bmiResult.configure(text=bmi)
+
+
+
+
+main = Tk()
+labelHeight = Label(main, text='Height (cm.)').grid(row=0, column=0)
+textBoxHeight = Entry(main)
+textBoxHeight.grid(row=0, column=1)
+labelWeight = Label(main, text='Weight (Kg.)').grid(row=1, column=0)
+textBoxWeight = Entry(main)
+textBoxWeight.grid(row=1, column=1)
+calculateButton = Button(main, text='Calculate')
+calculateButton.grid(row=2)
+calculateButton.bind('<Button-1>',leftClick)
+bmiResult = Label(main, text='Your BMI is:')
+bmiResult.grid(row=2, column=1)
+labelResult = Label(main, text='Result')
+labelResult.grid(row=3, column=1)
+main.mainloop()
